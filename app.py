5eeb0c040d98d75d0799fbc2f54b954d6afb9cfe
Robin van Leeuwen <robinvanleeuwen@gmail.com>
2021-05-02 14:55:56 +0200

    Update template

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 
 from flask_login import LoginManager
 from flask import Flask, render_template
+from flask_cors import CORS
 from flask_sqlalchemy_session import flask_scoped_session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -14,14 +15,19 @@ login_manager = LoginManager()
 base_dir = os.getcwd()
 
 app = Flask(__name__)
-login_manager.init_app(app)
-login_manager.login_view = 'stream'
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-engine = create_engine(os.environ['DATABASE_URL'])
-session_factory = sessionmaker(bind=engine)
-db_session = flask_scoped_session(session_factory, app)
+CORS(app)
+
+log.warning("LoginManager is not setup!")
+# login_manager.init_app(app)
+# login_manager.login_view = 'stream'
+
+log.warning("SQLAlchemy is not setup!")
+# engine = create_engine(os.environ['DATABASE_URL'])
+# session_factory = sessionmaker(bind=engine)
+# db_session = flask_scoped_session(session_factory, app)
 
 
 @app.route("/")
